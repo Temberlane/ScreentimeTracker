a5280d379ee0fb71a9814b0ae0a11b6a8a69dcf6
Temberlane <60454907+Temberlane@users.noreply.github.com>
2024-11-15 11:09:42 -0500

    initial commit

---
+++ Screen Time Tracker/Screen_Time_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Screen_Time_TrackerApp.swift
+//  Screen Time Tracker
+//
+//  Created by Thomas Li on 2024-11-15.
+//
+
+import SwiftUI
+
+@main
+struct Screen_Time_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Screen Time TrackerTests/Screen_Time_TrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Screen_Time_TrackerTests.swift
+//  Screen Time TrackerTests
+//
+//  Created by Thomas Li on 2024-11-15.
+//
+
+import Testing
+@testable import Screen_Time_Tracker
+
+struct Screen_Time_TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
